2b041826732da0fea71b221bc0e8b19ae6b3f6ca
Dev Inoue <pasteur1822@gmail.com>
2018-10-28 00:10:11 +0900

    Add Restful API routing

--- app/Http/Controllers/YourProgressController.php
+++ app/Http/Controllers/YourProgressController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Todos;
+
 class YourProgressController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('index');
+    }
+    public function create()
+    {
+
+    }
+    public function store(Request $request)
+    {
+
+    }
+    public function show()
+    {
+        //
+    }
+    public function update(Request $request, $id)
+    {
+
+    }
+    public function destroy()
+    {
+
+    }
+    public function edit()
+    {
+        //
     }
 }
