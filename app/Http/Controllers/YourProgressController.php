7dad67b9244501e12f293e46dae4ae792b432b33
Dev Inoue <pasteur1822@gmail.com>
2018-10-27 23:14:37 +0900

    Add index method.

--- app/Http/Controllers/YourProgressController.php
+++ app/Http/Controllers/YourProgressController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 use App\Todos;
 class YourProgressController extends Controller
 {
-    //
+    public function index(){
+        return view('index');
+    }
 }
--- database/migrations/2018_10_27_111123_create_todos_table.php
+++ database/migrations/2018_10_27_111123_create_todos_table.php
@@ -14,7 +14,11 @@ class CreateTodosTable extends Migration
     public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
-            $table->increments('id');
+            $table->string('p_id');
+            $table->string('id_name')->primary();
+            $table->string('task_name');
+            $table->integer('progress')->unsigned();
+            $table->integer('state');
             $table->timestamps();
         });
     }
